ed20935d7df621ce0e905a678510da447705431c
Dzhivanyan <d.david@intervolga.ru>
2024-05-28 21:53:05 +0300

    [develop] - slider content

--- src/blocks.04-project/main-block/main-block.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/main-block/main-block.tmpl-specs/base.bemjson.js
@@ -9,7 +9,7 @@ module.exports = [
           {elem: 'copy-text', content: '0x0781694823f34e3bcbc8192e668960cc96163eb7'},
           {block: 'img', cls: 'd-none d-sm-block', src: './images/main-block/copy.png'},
         ]},
-      {block: 'btn', content: 'Powerful for developers. <br>Fast for everyone.'},
+      {block: 'btn', content: 'Run for your dreams. <br>Solana Sneakers.'},
       {elem: 'text', content: 'Short sellin jeets not stopping anything'}
     ]}
 ];
--- src/blocks.04-project/main-slider/main-slider.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/main-slider/main-slider.tmpl-specs/base.bemjson.js
@@ -1,14 +1,14 @@
 module.exports = [
   {block: 'main-slider', cls: 'swiper', content: [
-      {cls: 'swiper-wrapper', content: [...new Array(2)].map((_, index) => [
-          {elem: 'slide', cls: 'swiper-slide', content: [
-              {elem: 'img', content: [
-                  {block: 'img', src: './images/main-slider/slide1.png'}
+      {cls: 'swiper-wrapper', content: [
+          {elem: 'slide', mods: {big: true}, cls: 'swiper-slide', content: [
+              {elem: 'img', mods: {big: true}, content: [
+                  {block: 'img', src: './images/main-slider/slide5.jpg'}
                 ]}
             ]},
             {elem: 'slide', mods: {double: true}, cls: 'swiper-slide', content: [
                 {elem: 'img', content: [
-                    {block: 'img', src: './images/main-slider/slide2.jpg'},
+                    {block: 'img', src: './images/main-slider/slide1.jpg'},
                   ]},
                 {elem: 'img', content: [
                     {block: 'img', src: './images/main-slider/slide2.jpg'},
@@ -19,70 +19,24 @@ module.exports = [
                   {block: 'img', src: './images/main-slider/slide3.jpg'}
                 ]}
             ]},
+          {elem: 'slide', cls: 'swiper-slide', content: [
+              {elem: 'img', content: [
+                  {block: 'img', src: './images/main-slider/slide6.jpg'}
+                ]}
+            ]},
           {elem: 'slide', mods: {double: true}, cls: 'swiper-slide', content: [
               {elem: 'img', content: [
-                  {block: 'img', src: './images/main-slider/slide2.jpg'},
+                  {block: 'img', src: './images/main-slider/slide1.jpg'},
                 ]},
               {elem: 'img', content: [
                   {block: 'img', src: './images/main-slider/slide2.jpg'},
                 ]},
             ]},
-        ])}
-
-
-      // {cls: 'swiper-wrapper', content: [
-      //     {elem: 'slide', cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide1.png'}
-      //           ]}
-      //       ]},
-      //     {elem: 'slide', mods: {even: true}, cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //       ]},
-      //     {elem: 'slide', cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide1.png'}
-      //           ]}
-      //       ]},
-      //     {elem: 'slide', mods: {even: true}, cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //       ]},
-      //     {elem: 'slide', cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide1.png'}
-      //           ]}
-      //       ]},
-      //     {elem: 'slide', mods: {even: true}, cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //       ]},
-      //     {elem: 'slide', cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide1.png'}
-      //           ]}
-      //       ]},
-      //     {elem: 'slide', mods: {even: true}, cls: 'swiper-slide', content: [
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //         {elem: 'img', content: [
-      //             {block: 'img', src: './images/main-slider/slide2.jpg'},
-      //           ]},
-      //       ]},
-      //   ]}
+          {elem: 'slide', mods: {big: true}, cls: 'swiper-slide', content: [
+              {elem: 'img', content: [
+                  {block: 'img', src: './images/main-slider/slide4.jpg'}
+                ]}
+            ]},
+        ]}
     ]}
 ];
